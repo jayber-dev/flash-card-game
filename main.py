3af7f7872bb56098983b397fbc33cf6b5b2e2fd2
jayber-dev <79035534+jayber-dev@users.noreply.github.com>
2021-05-02 19:59:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,30 @@ BACKGROUND_COLOR = "#B1DDC6"
 import pandas
 from tkinter import *
 
+# ------------------------------- FRONT CARD ----------------------------------- #
+
+
+
+# ------------------------------- BACK CARD ------------------------------------- #
+
+
+
+
+# --------------------------------------USER UI --------------------------------- #
+
+window = Tk()
+window.config(bg="#B1DDC6")
+# right_img = PhotoImage(file="/images/right.png")
+# wrong_img = PhotoImage(file="/images/wrong.png")
+front_card_import = PhotoImage(file="images/card_front.png")
+back_card = PhotoImage(file="images/card_back.png")
+
+canvas = Canvas(width=800,height=526)
+
+canvas.create_image(400, 263, image=front_card_import)
+canvas.grid(row=0,column=0)
+
+ 
+
+window.mainloop()
 
